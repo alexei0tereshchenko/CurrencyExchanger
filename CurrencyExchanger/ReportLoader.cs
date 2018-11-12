190b2766cfe4ab2b1a49a31002bc2b9262b86aa0
alexei0tereshchenko <alexei0tereshchenko@gmail.com>
2018-11-12 18:00:48 +0300

    Added Report list to Administrator Window

---
+++ CurrencyExchanger/ReportLoader.cs
@@ -0,0 +1,19 @@
+﻿using CurrencyExchanger.Content;
+using FirstFloor.ModernUI.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchanger
+{
+    class ReportLoaderDefaultContentLoader : DefaultContentLoader
+    {
+        protected override object LoadContent(Uri uri)
+        {
+            // return a new Report user control instance no matter the uri
+            return new Report();
+        }
+    }
+}
