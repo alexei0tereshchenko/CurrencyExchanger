cb18f2adbda56ad36e833978891c3fcf8e81c8db
alexei0tereshchenko <alexei0tereshchenko@gmail.com>
2018-12-14 12:52:13 +0300

    Removed db credentials from source

--- CurrencyExchanger/packages/model/currencyexchangerContext.cs
+++ CurrencyExchanger/packages/model/currencyexchangerContext.cs
@@ -24,7 +24,7 @@ namespace CurrencyExchanger.packages.model
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:currencyexchanger.database.windows.net,1433;Initial Catalog=currencyexchanger;Persist Security Info=False;User ID=alexei0tereshchenko;Password=Lyosha_19;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:currencyexchanger.database.windows.net,1433;Initial Catalog=currencyexchanger;Persist Security Info=False;User ID=*******;Password=*******;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
