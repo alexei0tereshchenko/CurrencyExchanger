afd37a22d183c2b4add41782fad49f853ad5c834
alexei0tereshchenko <alexei0tereshchenko@gmail.com>
2018-12-22 12:00:05 +0300

    Made a little fix

--- User/bo/UpdateEmployeeBO.cs
+++ User/bo/UpdateEmployeeBO.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using Abstract.bo.@abstract;
-using User = Abstract.model.User;
-
 namespace User.bo
 {
     public class UpdateEmployeeBO:AbstractUpdateBO
